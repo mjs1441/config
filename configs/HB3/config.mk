135f828e97189967f37be0c186f2149c01428fae
Matthew Selby <matthew.selby@raspberrypi.com>
2026-01-15 19:02:13 +0000

    HB3 config.mk set MCU_FLASH_SIZE and PICO_FLASH etc.

--- configs/HB3/config.mk
+++ configs/HB3/config.mk
@@ -1,3 +1,9 @@
 # It's possibile to set makefile definitions here.
 # Originally used to configure flash size and font allocation, but that's now in pico_flash_mem.ld in this folder.
 
+MCU_FLASH_SIZE = 2048
+# 2097152 = 2 * 1024 * 1024
+TARGET_DEVICE_FLAGS  = \
+                   -DPICO_FLASH_SPI_CLKDIV=2 \
+                   -DPICO_FLASH_SIZE_BYTES=2097152 \
+                   -DPICO_BOOT_STAGE2_CHOOSE_W25Q080=1
